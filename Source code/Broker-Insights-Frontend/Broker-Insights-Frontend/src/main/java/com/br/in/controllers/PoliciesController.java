cbf0515fcf3ca65dbaa41f34f1a0e21e228ab7eb
Paul van Sittert <paul.d.vansittert@gmail.com>
2022-08-26 05:01:49 +0100

    Comments added

--- Source code/Broker-Insights-Frontend/Broker-Insights-Frontend/src/main/java/com/br/in/controllers/PoliciesController.java
+++ Source code/Broker-Insights-Frontend/Broker-Insights-Frontend/src/main/java/com/br/in/controllers/PoliciesController.java
@@ -23,50 +23,61 @@ public class PoliciesController {
 	@Autowired
 	PolicyService policyService;
 	
+	//Function for when users access the Homepage.
 	@RequestMapping("/")
 	public ModelAndView loadInHomePage() {
 		ModelAndView modelAndView = new ModelAndView();
 				
+		//Loads in the list of clients into the drop down menu
 		ClientList clientList = policyService.getClientList();
+		modelAndView.addObject("ClientList", clientList.getClients());
+		
+		//Sets up the c;ient object for when users press the search Button to select a client.
 		Client client = new Client();
 		modelAndView.addObject("Client", client);
 		
-		modelAndView.addObject("ClientList", clientList.getClients());
 		modelAndView.setViewName("index");
 		
+		//Creates a blank CustomerPolicy, required so that the Div that would contain all the loaded policies doesnt throw errors
 		ArrayList<CustomerPolicy> temp = new ArrayList<CustomerPolicy>();
 		modelAndView.addObject("customerPolicies", temp);
 		
 		return modelAndView;
 	}
 	
+	//Function for when the user searches for a specific clients policies.
 	@RequestMapping("/searchClientsPolicies")
 	public ModelAndView addPolicy(Client currentClient) {
 		ModelAndView modelAndView = new ModelAndView();
 		
+		
+		//Loads in the list of clients into the drop down menu
 		ClientList clientList = policyService.getClientList();
+		modelAndView.addObject("ClientList", clientList.getClients());
+				
+		//Sets up the client object for when users press the search Button to select a client.
 		Client client = new Client();
 		modelAndView.addObject("Client", client);
-		modelAndView.addObject("ClientList", clientList.getClients());
+				
 		modelAndView.setViewName("index");
 		
-		
+		//Uses the name of the current client to get a list of policies
 		CustomerPolicyList customerPolicyList = policyService.getClientPolicies(currentClient.getClient_name());
 		modelAndView.addObject("customerPolicies", customerPolicyList.getCustomerPolicy());
 		
+		//Adds the clients name to specific parts of the page to pass it further along.
 		modelAndView.addObject("client_name", currentClient.getClient_name());
-		
-		
-		modelAndView.setViewName("index");
 		
 		return modelAndView;
 	}
 	
-	
+	//Function for when users select the add policy button. Takes users to another page where they can fill out data to create
+	//a new policy.
 	@RequestMapping("/addPolicy/{client_name}")
 	public ModelAndView addPolicy(@PathVariable String client_name) {
 		ModelAndView modelAndView = new ModelAndView();
 		
+		//Creates an empty CustomerPolicy object but only saves the client name as that is the only current variable.
 		CustomerPolicy customerPolicy = new CustomerPolicy();
 		customerPolicy.setClient_name(client_name);
 		modelAndView.addObject("Policy", customerPolicy);
@@ -75,35 +86,43 @@ public class PoliciesController {
 		return modelAndView;
 	}
 	
+	//Function for when users have filled out the policy creation form and pressed the submit button.
 	@RequestMapping("/createPolicy")
 	public ModelAndView createPolicy(CustomerPolicy customerPolicy) {
 		ModelAndView modelAndView = new ModelAndView();
 		
+		//Saves the newly created Policy
 		policyService.uploadNewPolicy(customerPolicy);
 		
+		//Loads in the list of clients into the drop down menu
 		ClientList clientList = policyService.getClientList();
+		modelAndView.addObject("ClientList", clientList.getClients());
+				
+		//Sets up the client object for when users press the search Button to select a client.
 		Client client = new Client();
 		modelAndView.addObject("Client", client);
-		modelAndView.addObject("ClientList", clientList.getClients());
+				
+		//Takes users back to the main page
 		modelAndView.setViewName("index");
 		
 		
+		//Uses the name of the current client to get a list of policies
 		CustomerPolicyList customerPolicyList = policyService.getClientPolicies(customerPolicy.getClient_name());
 		modelAndView.addObject("customerPolicies", customerPolicyList.getCustomerPolicy());
-		
+				
+		//Adds the clients name to specific parts of the page to pass it further along.
 		modelAndView.addObject("client_name", customerPolicy.getClient_name());
 		
-		
-		modelAndView.setViewName("index");
 		
 		return modelAndView;
 	}
 	
-	
+	//Function for when users selects the modify policy button. Takes user to another page where they can modify the policy data.
 	@RequestMapping("/modifyPolicy/{id}")
 	public ModelAndView modifyPolicy(@PathVariable int id) {
 		ModelAndView modelAndView = new ModelAndView();
 		
+		//Using the Policy id, load in the policy data and pre-fill the form data.
 		CustomerPolicy customerPolicy = policyService.getPolicy(id);
 		
 		modelAndView.addObject("Policy", customerPolicy);
@@ -113,27 +132,32 @@ public class PoliciesController {
 		return modelAndView;
 	}
 	
-	
+	//Function for when users have finished modifying the policy data and press the submit button.
 	@RequestMapping("/submitModifiedPolicy")
 	public ModelAndView submitModifiedPolicy(CustomerPolicy customerPolicy) {
 		ModelAndView modelAndView = new ModelAndView();
 		
+		//Saves the newly modified Policy
 		policyService.modifyPolicy(customerPolicy);
 		
+		//Loads in the list of clients into the drop down menu
 		ClientList clientList = policyService.getClientList();
+		modelAndView.addObject("ClientList", clientList.getClients());
+				
+		//Sets up the client object for when users press the search Button to select a client.
 		Client client = new Client();
 		modelAndView.addObject("Client", client);
-		modelAndView.addObject("ClientList", clientList.getClients());
+				
+		//Takes users back to the main page
 		modelAndView.setViewName("index");
-		
-		
+				
+				
+		//Uses the name of the current client to get a list of policies
 		CustomerPolicyList customerPolicyList = policyService.getClientPolicies(customerPolicy.getClient_name());
 		modelAndView.addObject("customerPolicies", customerPolicyList.getCustomerPolicy());
-		
+						
+		//Adds the clients name to specific parts of the page to pass it further along.
 		modelAndView.addObject("client_name", customerPolicy.getClient_name());
-		
-		
-		modelAndView.setViewName("index");
 		
 		return modelAndView;
 	}
--- Source code/Broker-Insights-Frontend/Broker-Insights-Frontend/src/main/java/com/br/in/service/PolicyServiceImpl.java
+++ Source code/Broker-Insights-Frontend/Broker-Insights-Frontend/src/main/java/com/br/in/service/PolicyServiceImpl.java
@@ -18,28 +18,34 @@ public class PolicyServiceImpl implements PolicyService {
 	@Autowired
 	private RestTemplate restTemplate;
 
+	//Gets a list of all clients from the backend.
 	@Override
 	public ClientList getClientList() {
 		return restTemplate.getForObject("http://localhost:8090/clientList", ClientList.class);
 	}
 
+	//Using the clients name gets all of the policies associated with that client.
 	@Override
 	public CustomerPolicyList getClientPolicies(String name) {
 		return restTemplate.getForObject("http://localhost:8090/clientPolicies/" +name, CustomerPolicyList.class);
 	}
 
+	//Uploads a CustomerPolicy object to the backend.
 	@Override
 	public CustomerPolicy uploadNewPolicy(CustomerPolicy customerPolicy) {
 		return restTemplate.postForObject("http://localhost:8090/createPolicy", customerPolicy, CustomerPolicy.class);
 	}
 
+	//Uploads a modified CustomerPolicy object to update and existing policy in the database.
 	@Override
 	public CustomerPolicy modifyPolicy(CustomerPolicy customerPolicy) {
+		//Manually uses the PUT method to upload the data as typically put requests do not receive any response.
 		HttpHeaders headers = new HttpHeaders();
 		HttpEntity<CustomerPolicy> entity = new HttpEntity<CustomerPolicy>(customerPolicy, headers);
 		return restTemplate.exchange("http://localhost:8090/modifyPolicy", HttpMethod.PUT, entity, CustomerPolicy.class).getBody();
 	}
 
+	//Gets the data of a specific policy using the id of the policy.
 	@Override
 	public CustomerPolicy getPolicy(int id) {
 		return restTemplate.getForObject("http://localhost:8090/getPolicy/" +id, CustomerPolicy.class);
